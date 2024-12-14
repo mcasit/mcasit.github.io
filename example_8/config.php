587e5062496727f46b5caf78e80a3d89d6059e27
mcasit <pgmca.sit@gmail.com>
2024-12-14 16:16:01 +0530

    Delete example_8 directory

--- example_8/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DATABASE_HOST", "localhost");
-define("DATABASE_NAME", "SIT");
-define("DATABASE_USER", "root");
-define("DATABASE_PASSWORD", "");
--- example_8/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Step: 6 Destroy session variable of that username 
-session_unset();
-session_destroy();
-// Step: 7 Redirect to login file
-header('location: login.php');
-?>
