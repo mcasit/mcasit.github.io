03bb7ec5692b2a0f8645846932ec47b1a230f553
mcasit <pgmca.sit@gmail.com>
2024-12-15 09:47:34 +0530

    Update helper_database.php

--- helper_database.php
+++ helper_database.php
@@ -1,8 +1,23 @@
 <?php
-//including required file
-require_once "./config.php";
 
-//connection function
+####################### MODULE: DATABASE HELPER MODULE #######################
+# 1. DATABASE CONNECTIVITY METHOD
+# 2. DATABASE CONNECTION CLOSE METHOD
+# 3. INSERT INTO TABLE METHOD
+# 4. GET DATA FROM TABLE METHOD
+# 5. UPDATE DATA FROM TABLE METHOD
+# 6. DELETE DATA FROM TABLE METHOD
+##############################################################################
+
+
+############### 1. DATABASE CONNECTIVITY METHOD ###############
+# DESCRIPTION: 
+#       Method to establish the MySQL database connectivity
+# 
+# PARAMETERS: None
+#
+# RETURN VALUES:
+#       mysqli connection object
 function establish_connection(): mysqli
 {
 
@@ -19,12 +34,33 @@ function establish_connection(): mysqli
 
     return $connection_object;
 }
+
+############### 2. DATABASE CONNECTION CLOSE METHOD ###############
+# DESCRIPTION: 
+#       Method to close the MySQL Connectivity
+# 
+# PARAMETERS:
+#       connection_object: mysql database connection object
+#
+# RETURN VALUES: None
+#     
 function close_database_connect($connection_object)
 {
     mysqli_close($connection_object);
 }
 
-// Step: 5 Create Helper function for Insert into MySQL
+
+############### 3. INSERT INTO TABLE METHOD ###############
+# DESCRIPTION: 
+#       Method to insert records into table
+# 
+# PARAMETERS:
+#       connection_object: mysql database connection object
+#       insert_query: string
+#
+# RETURN VALUES:
+#       last_id : int
+
 function insert_data($connection_object, $insert_query): int
 {
 
@@ -39,7 +75,18 @@ function insert_data($connection_object, $insert_query): int
     return $last_id;
 }
 
-// Step: 8 Create Helper function for Selecting data from MySQL
+
+############### 4. GET DATA FROM TABLE METHOD ###############
+# DESCRIPTION: 
+#       Method to get the data from Table
+# 
+# PARAMETERS:
+#       connection_object: mysql database connection object
+#       insert_query: string
+#
+# RETURN VALUES:
+#       result_set : array of records
+
 function get_table_data($connection_object, $select_query): array
 {
 
@@ -52,7 +99,17 @@ function get_table_data($connection_object, $select_query): array
     return $result_set;
 }
 
-// Step: 19 Create Helper function for Update into MySQL
+############### 5. UPDATE DATA FROM TABLE METHOD ###############
+# DESCRIPTION: 
+#       Method to update the data in Table
+# 
+# PARAMETERS:
+#       connection_object: mysql database connection object
+#       update_query: string
+#
+# RETURN VALUES: Bool
+#
+
 function update_table_data($connection_object, $update_query): bool
 {
 
@@ -63,7 +120,17 @@ function update_table_data($connection_object, $update_query): bool
     return false;
 }
 
-// Step: 24 Create Helper function for Delete into MySQL
+############### 6. DELETE DATA FROM TABLE METHOD ###############
+# DESCRIPTION: 
+#       Method to DELETE the data in Table
+# 
+# PARAMETERS:
+#       connection_object: mysql database connection object
+#       delete_query: string
+#
+# RETURN VALUES: None
+#
+
 function delete_table_data($connection_object, $delete_query): bool
 {
 
@@ -73,5 +140,3 @@ function delete_table_data($connection_object, $delete_query): bool
     }
     return false;
 }
-
-?>
